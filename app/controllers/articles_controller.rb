cd0aa3ce4d686eea2c70dc18958123cfd8b2e7bf
Sven Seemann <sven.seemann@bindoc.de>
2015-06-23 12:47:08 +0200

    creating articles works now, have to be logged in to see, belongs to #1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,27 @@
 class ArticlesController < ApplicationController
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+
+    unless(@article.save)
+      #TODO: show error page
+    end
+
+    redirect_to article_path(@article)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private # --------------------------------------------------------------------
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
