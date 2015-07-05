5c22b22ef3fd062f1de59129acad6d6cca279cc4
Sven Seemann <sven.seemann@bindoc.de>
2015-07-05 10:31:39 +0200

    changed repo url for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'blublisher'
-set :repo_url, 'ssh://git@github.com:SvenSeemann/blublisher.git'
+set :repo_url, 'git@github.com:SvenSeemann/blublisher.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
