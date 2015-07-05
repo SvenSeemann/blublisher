538951eb096c0dedb8293f456a7c136189e0a7e7
Sven Seemann <sven.seemann@bindoc.de>
2015-07-05 13:36:21 +0200

    added config file for puma

---
+++ config/puma.rb
@@ -0,0 +1,18 @@
+workers 1
+
+threads 1, 1
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "/var/www/blublisher/shared"
+
+bind "unix://#{shared_dir}/sockets/puma.sock"
+
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+
+activate_control_app
+on_worker_boot do
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")["production"])
+end
