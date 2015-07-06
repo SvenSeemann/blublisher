51711081b4b5a033d8090eb12dafd0a994f5e24f
Sven Seemann <sven.seemann@bindoc.de>
2015-07-06 20:46:17 +0200

    removed local_precompile gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,6 +32,31 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # set :keep_releases, 5
 
 namespace :deploy do
+  namespace :assets do
+
+    Rake::Task['deploy:assets:precompile'].clear_actions
+
+    desc 'Precompile assets locally and upload to servers'
+    task :precompile do
+      on roles(fetch(:assets_roles)) do
+        run_locally do
+          with rails_env: fetch(:rails_env) do
+            execute 'bin/rake assets:precompile'
+          end
+        end
+
+        within release_path do
+          with rails_env: fetch(:rails_env) do
+            upload!('./public/assets/', "#{shared_path}/public/", recursive: true)
+          end
+        end
+
+        run_locally { execute 'rm -rf public/assets' }
+      end
+    end
+
+  end
+
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 1, wait: 10 do
       within release_path do
