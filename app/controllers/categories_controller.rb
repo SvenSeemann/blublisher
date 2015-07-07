fc11f93ec39108f6465790a1521e37e1c8fbbf5b
Sven Seemann <sven.seemann@bindoc.de>
2015-07-07 11:49:44 +0200

    categories are destroyable

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
 
-  before_action :authenticate_user!, except: [:index, :show, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show, :new, :create, :destroy]
 
   def index
     @categories = Category.all
@@ -27,6 +27,16 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
     render layout: "admin"
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    respond_to do |format|
+      format.html { redirect_to categories_url, notice: 'Kategorie erfolgreich gelöscht.' }
+      format.json { head :no_content }
+    end
   end
 
 
